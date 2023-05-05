7924b9ebcbc1b60243a9eaaf49c670024b72de2d
Saon00 <sikdersaon1@gmail.com>
2023-05-05 13:00:18 +0600

    drawer page done

--- lib/components/color.dart
+++ lib/components/color.dart
@@ -2,4 +2,5 @@ import 'package:flutter/material.dart';
 
 class CustomColor {
   static Color BARBG = Colors.blueGrey.shade800;
+  static Color ICONCOLOR = Colors.white;
 }
--- lib/components/my_drawer.dart
+++ lib/components/my_drawer.dart
@@ -1,12 +1,73 @@
+import 'package:fecthesispublish/components/color.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class MyDrawer extends StatelessWidget {
   const MyDrawer({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Drawer(
-      
+    double resWIDTH = MediaQuery.of(context).size.width;
+    return SafeArea(
+      child: Drawer(
+        width: resWIDTH * 0.6,
+        backgroundColor: CustomColor.BARBG,
+        child: Padding(
+          padding: const EdgeInsets.all(10.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              // fec logo
+              Column(
+                children: [
+                  Image.asset('assets/fec_logo.png'),
+
+                  // contact
+                  ListTile(
+                    leading: FaIcon(
+                      FontAwesomeIcons.addressCard,
+                      color: CustomColor.ICONCOLOR,
+                    ),
+                    title: Text(
+                      'Contact',
+                      style: GoogleFonts.ubuntu(color: CustomColor.ICONCOLOR),
+                    ),
+                  ),
+
+                  // Settings
+                  ListTile(
+                    leading: FaIcon(FontAwesomeIcons.gear,
+                        color: CustomColor.ICONCOLOR),
+                    title: Text(
+                      'Settings',
+                      style: GoogleFonts.ubuntu(color: CustomColor.ICONCOLOR),
+                    ),
+                  ),
+
+                  // Our website
+                  ListTile(
+                    leading: FaIcon(FontAwesomeIcons.earthAsia,
+                        color: CustomColor.ICONCOLOR),
+                    title: Text(
+                      'Our Website',
+                      style: GoogleFonts.ubuntu(color: CustomColor.ICONCOLOR),
+                    ),
+                  ),
+                ],
+              ),
+
+              ListTile(
+                  leading: FaIcon(FontAwesomeIcons.rightFromBracket,
+                      color: CustomColor.ICONCOLOR),
+                  title: Text(
+                    'Log-Out',
+                    style: GoogleFonts.ubuntu(color: CustomColor.ICONCOLOR),
+                  ))
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
