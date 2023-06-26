11511dfe459d1682116fe69eac593f1a4c7a4121
Saon00 <sikdersaon1@gmail.com>
2023-06-27 00:40:29 +0600

    batch and dept sort added

--- lib/screens/papers_screen.dart
+++ lib/screens/papers_screen.dart
@@ -1,6 +1,7 @@
 import 'package:fecthesispublish/screens/submission_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 import '../components/color.dart';
 
@@ -12,8 +13,29 @@ class PapersScreen extends StatefulWidget {
 }
 
 class _PapersScreenState extends State<PapersScreen> {
-  final TextEditingController searchText = TextEditingController();
-  late String show = "";
+  final TextEditingController searchController = TextEditingController();
+  late String searchText = "";
+
+  List<String> batchDropDownItem = <String>[
+    'Batch-01',
+    'Batch-02',
+    'Batch-03',
+    'Batch-04',
+    'Batch-05',
+    'Batch-06',
+    'Batch-07',
+    'Batch-08',
+    'Batch-09',
+    'Batch-10',
+  ];
+  String batchDropDownValue = 'Batch-01';
+
+  List<String> deptDropDownItem = <String>[
+    'CSE',
+    'EEE',
+    'CE',
+  ];
+  String deptDropDownValue = 'CSE';
 
   @override
   Widget build(BuildContext context) {
@@ -32,18 +54,21 @@ class _PapersScreenState extends State<PapersScreen> {
         child: Padding(
           padding: const EdgeInsets.all(24.0),
           child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               Row(
                 children: [
+                  // department filter
                   SizedBox(
                     width: resWIDTH * 0.7,
                     child: TextFormField(
-                      controller: searchText,
+                      controller: searchController,
                       // onChanged: (value) => show = searchText.toString(),
                       decoration: InputDecoration(
                         // prefixIcon: const Icon(Icons.search),
                         // prefixIconColor: CustomColor.BLUEGREY,
                         hintText: 'search.....',
+                        hintStyle: GoogleFonts.ubuntu(),
                         border: OutlineInputBorder(
                           borderRadius: BorderRadius.circular(10),
                         ),
@@ -54,8 +79,8 @@ class _PapersScreenState extends State<PapersScreen> {
                   OutlinedButton(
                     onPressed: () {
                       setState(() {
-                        show = searchText.text;
-                        searchText.clear();
+                        searchText = searchController.text;
+                        searchController.clear();
                       });
                     },
                     child: SizedBox(
@@ -68,7 +93,88 @@ class _PapersScreenState extends State<PapersScreen> {
                   ),
                 ],
               ),
+              const SizedBox(height: 20),
               // Text(show),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  // batch filter
+                  SizedBox(
+                    width: resWIDTH * 0.3,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          "Department",
+                          style: GoogleFonts.ubuntu(fontSize: 18),
+                        ),
+                        const SizedBox(height: 10),
+
+                        // department select
+                        DropdownButtonFormField<String>(
+                          hint: const Text('Select Department'),
+                          dropdownColor: Colors.blueGrey.shade200,
+                          isExpanded: true,
+                          decoration: InputDecoration(
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(10),
+                            ),
+                          ),
+                          items: deptDropDownItem.map((String value) {
+                            return DropdownMenuItem<String>(
+                              value: value,
+                              child: Text(value, style: GoogleFonts.ubuntu()),
+                            );
+                          }).toList(),
+                          onChanged: (String? newValue) {
+                            setState(() {
+                              deptDropDownValue = newValue!;
+                            });
+                          },
+                          value: deptDropDownValue,
+                        ),
+                      ],
+                    ),
+                  ),
+                  SizedBox(
+                    width: resWIDTH * 0.5,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          "Batch",
+                          style: GoogleFonts.ubuntu(fontSize: 18),
+                        ),
+                        const SizedBox(height: 10),
+
+                        // batch select
+                        DropdownButtonFormField<String>(
+                          hint: const Text('Select Batch'),
+                          dropdownColor: Colors.blueGrey.shade200,
+                          isExpanded: true,
+                          decoration: InputDecoration(
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(10),
+                            ),
+                          ),
+                          items: batchDropDownItem.map((String value) {
+                            return DropdownMenuItem<String>(
+                              value: value,
+                              child: Text(value, style: GoogleFonts.ubuntu()),
+                            );
+                          }).toList(),
+                          onChanged: (String? newValue) {
+                            setState(() {
+                              batchDropDownValue = newValue!;
+                            });
+                          },
+                          value: batchDropDownValue,
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
             ],
           ),
         ),
