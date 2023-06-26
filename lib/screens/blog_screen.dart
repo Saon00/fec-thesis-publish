e9913566c33e5bbeaae1a82b7da10aa3dada29fa
Saon00 <sikdersaon1@gmail.com>
2023-06-27 00:02:46 +0600

    search bar added

--- lib/screens/blog_screen.dart
+++ lib/screens/blog_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class BlogScreen extends StatefulWidget {
   const BlogScreen({super.key});
@@ -10,6 +11,29 @@ class BlogScreen extends StatefulWidget {
 class _BlogScreenState extends State<BlogScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: SingleChildScrollView(
+          scrollDirection: Axis.vertical,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              // profile
+              Text(
+                "Blogs",
+                style: GoogleFonts.ubuntu(fontSize: 40),
+              ),
+              const Divider(
+                thickness: 3,
+                endIndent: 200,
+                color: Colors.grey,
+              ),
+              const SizedBox(height: 20),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
--- lib/screens/papers_screen.dart
+++ lib/screens/papers_screen.dart
@@ -12,8 +12,12 @@ class PapersScreen extends StatefulWidget {
 }
 
 class _PapersScreenState extends State<PapersScreen> {
+  final TextEditingController searchText = TextEditingController();
+  late String show = "";
+
   @override
   Widget build(BuildContext context) {
+    double resWIDTH = MediaQuery.of(context).size.width;
     return Scaffold(
       // floatingActionButton
       floatingActionButton: FloatingActionButton(
@@ -22,6 +26,52 @@ class _PapersScreenState extends State<PapersScreen> {
           Get.to(const SubmissionScreen());
         },
         child: const Icon(Icons.add),
+      ),
+
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(24.0),
+          child: Column(
+            children: [
+              Row(
+                children: [
+                  SizedBox(
+                    width: resWIDTH * 0.7,
+                    child: TextFormField(
+                      controller: searchText,
+                      // onChanged: (value) => show = searchText.toString(),
+                      decoration: InputDecoration(
+                        // prefixIcon: const Icon(Icons.search),
+                        // prefixIconColor: CustomColor.BLUEGREY,
+                        hintText: 'search.....',
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                      ),
+                    ),
+                  ),
+                  const Spacer(),
+                  OutlinedButton(
+                    onPressed: () {
+                      setState(() {
+                        show = searchText.text;
+                        searchText.clear();
+                      });
+                    },
+                    child: SizedBox(
+                      height: 60,
+                      child: Icon(
+                        Icons.search,
+                        color: CustomColor.BLUEGREY,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              // Text(show),
+            ],
+          ),
+        ),
       ),
     );
   }
